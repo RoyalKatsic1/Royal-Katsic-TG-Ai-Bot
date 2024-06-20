73e84620bbb8cf8d80ff8c3fe927ad46f00422f0
RoyalKatsic1 <malingamark30@gmail.com>
2024-06-21 01:14:30 +0300

    Create Gunicorn.conf.py

---
+++ Gunicorn.conf.py
@@ -0,0 +1,7 @@
+     import os
+
+     bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
+     workers = 2  # Adjust based on your needs 
+
+     # Specify the correct module and callable
+     wsgi_app = "main:app"
