95366f95ff8ce21a7fa5440811b92d5d1a487ac9
RoyalKatsic1 <malingamark30@gmail.com>
2024-06-21 04:45:34 +0300

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+# Configuration file for Gunicorn
+
+bind = "0.0.0.0:{}".format(os.environ.get("PORT", 3000))  # Bind to all interfaces and extracted port
+workers = 3  # Adjust the number of worker processes as needed
+
+# Optional configurations (refer to Gunicorn documentation for details):
+# accesslog = "-"  # Log to standard output
+# errorlog = "-"  # Log errors to standard output
+# log_level = "info"  # Set logging level
+
+# The application object to be used by Gunicorn
+wsgi_app = "Main:app"
