f3afd80292846da89bb9f07ea8f956474762af17
RoyalKatsic1 <malingamark30@gmail.com>
2024-06-21 23:10:01 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -24,33 +24,6 @@ def create_table(conn):
     """
     try:
         c = conn.cursor()
-        c.execute()
-import sqlite3
-from sqlite3 import Error
-
-
-def create_connection(db_file):
-    """create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return conn
-
-
-def create_table(conn):
-    """create a table from the create_table_sql statement
-    :param conn: Connection object
-    :return:
-    """
-    try:
-        c = conn.cursor()
         c.execute(
             """
             CREATE TABLE IF NOT EXISTS conversations (
@@ -63,6 +36,7 @@ def create_table(conn):
         )
     except Error as e:
         print(e)
+
 
 def create_conversation(conn, conversation):
     """
