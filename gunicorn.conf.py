7198822a1acb639637423e613e0bdae9dac6afd6
RoyalKatsic1 <malingamark30@gmail.com>
2024-06-21 03:45:26 +0300

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import os.environ.get
-
-     bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
-     workers = 2  # Adjust based on your needs 
-
-     # Specify the correct module and callable
-     wsgi_app = "Main:app"
